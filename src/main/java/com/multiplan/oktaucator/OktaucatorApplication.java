cba493a2c2b4ec6aba0ac7cf4a8a900c29870816
Khandavilli <surya.khandavilli@multiplan.com>
2022-07-04 18:54:28 +0530

    first commit

---
+++ src/main/java/com/multiplan/oktaucator/OktaucatorApplication.java
@@ -0,0 +1,13 @@
+package com.multiplan.oktaucator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaucatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaucatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiplan/oktaucator/OktaucatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplan.oktaucator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaucatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
